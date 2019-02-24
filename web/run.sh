d13e3eb71f63eee685a953f2b3add65b92d5caeb
tom <tom@meadon.me>
2019-02-24 07:30:20 +0000

    bugfix

--- web/run.sh
+++ web/run.sh
@@ -4,5 +4,5 @@ info "Starting php-fpm"
 php-fpm --fpm-config /etc/php-fpm.d/wordpress.conf &
 
 info "Starting nginx"
-nginx -c /etc/nginx/conf.d/default.conf g "daemon off;" &
+nginx -c /etc/nginx/conf.d/default.conf -g "daemon off;" &
 wait
